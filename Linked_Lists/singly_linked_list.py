a2d26ec4c8e0c85e713d65f1355b4b3f4532ce7b
HEMANT KUMAR <hkbiet@gmail.com>
2024-07-30 18:22:09 +0530

    Update singly_linked_list.py
    Added reeversing

--- Linked_Lists/singly_linked_list.py
+++ Linked_Lists/singly_linked_list.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -18,14 +24,13 @@ class LinkedList:
             previous.next = current.next
 
     def insert_end(self, value):
-        new_node = Node()
-        new_node.data = value
+        new_node = Node(value)
         if not self.head:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
-            self.tail = new_node
+            self.tail = self.tail.next
 
     def print_list(self):
         values = []
@@ -35,11 +40,51 @@ class LinkedList:
             pointer = pointer.next
         print("->".join(values))
 
+    def reverse(self):
+        previous = None
+        current = self.head
+        next = current.next
 
-class Node:
-    def __init__(self):
-        self.data = None
-        self.next = None
+        while next:
+            current.next = previous
+            previous = current
+            current = next
+            next = current.next
+        current.next = previous
+        print("Printing  : ")
+        values = []
+        tempo = current
+        while tempo:
+            values.append(str(tempo.data))
+            tempo = tempo.next
+        print("->".join(values))
+
+        return
+
+    def reverse_store(self):
+        store = []
+        traveller = self.head
+        while traveller:
+            store.append(traveller.data)
+            traveller = traveller.next
+
+        temp_head = None
+        temp_tail = None
+        for item in reversed(store):
+            if not temp_head:
+                temp_head = Node(item)
+                temp_tail = temp_head
+            else:
+                temp_tail.next = Node(item)
+                temp_tail = temp_tail.next
+        print("Printing  : ")
+        values = []
+        tempo = temp_head
+        while tempo:
+            values.append(str(tempo.data))
+            tempo = tempo.next
+        print("->".join(values))
+        return temp_head
 
 
 singly_linked_list = LinkedList()
@@ -50,5 +95,9 @@ singly_linked_list.insert_end(4)
 
 singly_linked_list.print_list()
 
-singly_linked_list.remove(1)
-singly_linked_list.print_list()
+# singly_linked_list.remove(1)
+# singly_linked_list.print_list()
+
+# singly_linked_list.reverse_store()
+singly_linked_list.reverse()
+# singly_linked_list.print_list()
